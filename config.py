68e6be3964a0ece3ed971817dadd4560a6cfb4a3
Hlystunov <m3svi3oca>
2018-02-09 02:33:42 +0300

    OrdererDict

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
 # -- coding utf-8 --
 token = '###################################'
 
+from collections import OrderedDict
+
 from returns import \
     inn_entity, inn_individual, ogrn_entity, ogrn_individual, \
     snils, full_name, birdthay, login, mail, phone, uuid, guid
 
-actions = {
-    'ИНН': inn_entity,
-    'ИНН ФЛ': inn_individual,
-    'ОГРН': ogrn_entity,
-    'ОГРН ИП': ogrn_individual,
-    'СНИЛС': snils,
-    'ФИО': full_name,
-    'Дата рождения': birdthay,
-    'Логин': login,
-    'E-mail': mail,
-    'Телефон': phone,
-    'GUID': guid,
-    'UUID': uuid,
-}
+actions = OrderedDict([
+    ('ИНН', inn_entity),
+    ('ИНН ФЛ', inn_individual),
+    ('ОГРН', ogrn_entity),
+    ('ОГРН ИП', ogrn_individual),
+    ('СНИЛС', snils),
+    ('ФИО', full_name),
+    ('Дата рождения', birdthay),
+    ('Логин', login),
+    ('E-mail', mail),
+    ('Телефон', phone),
+    ('GUID', guid),
+    ('UUID', uuid),
+])
